413d101f05c33bacc8a180021bfcee5a59132812
Ankit Singh <ankitsinghmyself@gmail.com>
2026-04-15 01:23:53 +0530

    mfe-remote-profile-init

---
+++ craco.config.js
@@ -0,0 +1,31 @@
+const { ModuleFederationPlugin } = require("webpack").container;
+
+module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.output.publicPath = "auto";
+webpackConfig.output.uniqueName = "profile";
+      webpackConfig.output.chunkLoadingGlobal = "profileChunkLoader";
+
+      webpackConfig.optimization.runtimeChunk = false;
+
+      webpackConfig.plugins.push(
+        new ModuleFederationPlugin({
+          name: "dashboard",
+          filename: "remoteEntry.js",
+library: { type: "var", name: "profile" },
+          exposes: {
+            "./App": "./src/App",
+          },
+shared: {
+            react: { singleton: true, requiredVersion: false },
+            "react-dom": { singleton: true, requiredVersion: false },
+            "react-router-dom": { singleton: true, requiredVersion: false },
+          },
+        })
+      );
+
+      return webpackConfig;
+    },
+  },
+};
